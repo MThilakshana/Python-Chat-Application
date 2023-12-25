e3a3edd5ca519ef5c96fd661812603981f02a591
MThilakshana <madurathilakshana999@gmail.com>
2023-12-25 21:27:08 +0530

    25/12

--- program.py
+++ program.py
@@ -3,3 +3,48 @@ import time
 import threading
 from tkinter import *
 
+#main Window 
+root = Tk()
+root.geometry("300x500")
+root.config(bg="White")
+
+def func():
+    pass
+
+def threadsendmsg():
+    pass
+
+startchatimage = PhotoImage(file='C:/Users/DELL/Desktop/Python/Chat appliction/button.png')
+
+buttons = Button(root,image=startchatimage,
+                 bg="white",
+                 borderwidth=0,
+                 cursor="hand2",
+                 command=func)
+buttons.place(x=60,y=10)
+
+message = StringVar()
+messageboxentry = Entry(root,
+                   textvariable=message,
+                   font=('calibre',10),
+                   border=2,
+                   width=32)
+messageboxentry.place(x=10,y=444)
+
+sendmessageimg = PhotoImage(file='C:/Users/DELL/Desktop/Python/Chat appliction/send.png')
+
+sendbutton = Button(root,
+                    image=sendmessageimg,
+                    borderwidth=0,
+                    bg="white",
+                    cursor="hand2",
+                    command=threadsendmsg)
+sendbutton.place(x=255,y=437)
+
+listbx = Listbox(root,
+                 height=20,
+                 width=43,
+                 border=2)
+listbx.place(x=15,y=80)
+
+root.mainloop()
